6c0cbc67d5ccc1bd6f550c071846d2e681629bef
Keitamidehyah <56815486+Keitamidehyah@users.noreply.github.com>
2020-02-27 19:38:02 +0900

    firebase-deploy
    What
    firebaseでのデプロイ

---
+++ firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDnaGrwgdjdmBEChpK7yFfe_Aklj5XvRaE",
+  authDomain: "react-chat-267d3.firebaseapp.com",
+  databaseURL: "https://react-chat-267d3.firebaseio.com",
+  projectId: "react-chat-267d3",
+  storageBucket: "react-chat-267d3.appspot.com",
+  messagingSenderId: "456189518076",
+  appId: "1:456189518076:web:2381ac3dffd42b5999b779"
+};
